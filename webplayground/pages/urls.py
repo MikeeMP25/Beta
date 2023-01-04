de3d20ed3551aa3c8d4a245b25169bf63df164fc
MikeeMP <jesus.libra619@gmail.com>
2023-01-04 12:42:56 -0600

    agregue una CBV de consulta otra vista diferente que el administrador

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from . import views
+from .views import PageDetailView, PagesListViews, PageCreateView
 
+"""
 urlpatterns = [
     path('', views.pages, name='pages'),
     path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
-]
+]"""
+
+pages_patterns = ([
+    path('', PagesListViews.as_view(), name="pages"),
+    path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name="page"),
+    path('create/', PageCreateView.as_view(), name="create"),
+], "pages")
+
+# como cambiamos a clase con modelos directos cuando mandamos la id por la url de int:page_id a
+# int:pk pk= lo detecta directamente como un atributo primary key
--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Page
-
+# estos son objetos django con funciones especificas que nos permite trabajar de forma mas rapida
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse, reverse_lazy
 # Create your views here.
+# E  ste es para una lista de respuesta
+
+
+# Cambiamos la estructura Function Based Views a Class Based Views
+# CBV nos permite trabajar desde clases para cargar los modelos y renderizar a la vistas he enviar datos de
+# una consulta objetos,listas diccionarios o tuplas
+class PagesListViews(ListView):
+    model = Page
+
+
+# Este es para un objeto de respuesta de uno
+class PageDetailView(DetailView):
+    model = Page
+
+
+# Esta clase es para hacer un crete desde front-end
+class PageCreateView(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
+    #def get_success_url(self):
+        #return reverse('pages:pages')
+
+"""
 def pages(request):
     pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
+    return render(request, pages/pages_.html, {'pages':pages})
+# Este es para un objeto de respuesta de uno
 
 def page(request, page_id, page_slug):
     page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
+    return render(request, pages/page_list.html, {'page':page})
+"""
