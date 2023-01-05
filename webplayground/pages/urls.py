0d4ec3c4bfd8fb18508b89a5a84bb1339b032b56
MikeeMP <jesus.libra619@gmail.com>
2023-01-04 18:10:26 -0600

    agregue el control de usarios en la vistas create, update and delete

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PageDetailView, PagesListViews, PageCreateView
+from .views import *
 
 """
 urlpatterns = [
@@ -11,6 +11,8 @@ pages_patterns = ([
     path('', PagesListViews.as_view(), name="pages"),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name="page"),
     path('create/', PageCreateView.as_view(), name="create"),
+    path('update/<int:pk>/', PageUpdateView.as_view(), name="update"),
+    path('dalete/<int:pk>/', PageDeleteView.as_view(), name="delete"),
 ], "pages")
 
 # como cambiamos a clase con modelos directos cuando mandamos la id por la url de int:page_id a
--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -3,15 +3,33 @@ from .models import Page
 # estos son objetos django con funciones especificas que nos permite trabajar de forma mas rapida
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
+from .forms import PageForm
+from django.shortcuts import redirect
+
+
 # Create your views here.
-# E  ste es para una lista de respuesta
+# aqui creamos la clase principal que operar el dispanchas
+class StaffRequiredMixin(object):
+    """Este mixin requerira que el usuario sea miembro el staff  """
+    # Aqui rescribimos un metodo ahora le damos ciertos permisos a la vista
+    # que solo los usuario logias y registrados pueden ver la vista create en caso contrario
+    # los renderiza al login..!!
+    @method_decorator(staff_member_required())
+    def dispatch(self, request, *args, **kwargs):
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 
+# Este es para una lista de respuesta
+""" ESTO ES EN MODO LECTURA"""
 # Cambiamos la estructura Function Based Views a Class Based Views
 # CBV nos permite trabajar desde clases para cargar los modelos y renderizar a la vistas he enviar datos de
 # una consulta objetos,listas diccionarios o tuplas
+
+
 class PagesListViews(ListView):
     model = Page
 
@@ -21,13 +39,35 @@ class PageDetailView(DetailView):
     model = Page
 
 
+""" ESTO ES EN MODO ESCRITURA Y LECTURA (MANIPULABLE O EDITABLE)"""
+
+
 # Esta clase es para hacer un crete desde front-end
+@method_decorator(staff_member_required(), name='dispatch')
 class PageCreateView(CreateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
     success_url = reverse_lazy('pages:pages')
-    #def get_success_url(self):
-        #return reverse('pages:pages')
+
+# def get_success_url(self):
+# return reverse('pages:pages')
+
+
+@method_decorator(staff_member_required(), name='dispatch')
+class PageUpdateView(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+
+@method_decorator(staff_member_required(), name='dispatch')
+class PageDeleteView(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
+
 
 """
 def pages(request):
